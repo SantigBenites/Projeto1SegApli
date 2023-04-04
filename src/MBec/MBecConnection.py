cb077fa91dc3883f202fbc410f20f9f807f30f50
SantigBenites <santi.mbenites@gmail.com>
2023-04-04 19:59:00 +0100

    Added things to README

--- src/MBec/MBecConnection.py
+++ src/MBec/MBecConnection.py
@@ -1,6 +1,19 @@
-import socket
+import socket, os, json
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import dh
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+
+current_working_directory = os.getcwd()
 
 def sendMessage(destIP:str, destPort:int, message: str):
+
+    userFilePath = f"{current_working_directory}/src/MBec/usersFiles/{json.loads(message.decode('utf8'))['account']}"
+    if not os.path.isfile(userFilePath):
+        #Execute the diffie hellman
+        parameters = dh.generate_parameters(generator=2, key_size=2048)
+        peer_private_key = parameters.generate_private_key()
+        
+    
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((destIP, destPort))
